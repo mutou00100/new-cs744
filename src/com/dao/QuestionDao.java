8c9d9304bef74d82b1a916be4a5e18c157ee0d46
aqianner <aqianner@gmail.com>
2016-03-29 01:22:23 -0500

    questionDao

--- src/com/dao/QuestionDao.java
+++ src/com/dao/QuestionDao.java
@@ -1,62 +1,81 @@
 package com.dao;
 
-	import java.sql.Connection;
-	import java.sql.PreparedStatement;
-	import java.sql.ResultSet;
-	import java.sql.SQLException;
-	import java.sql.Statement;
-	import java.util.ArrayList;
-	import java.util.Iterator;
-	import java.util.List;
-	import java.util.Map;
-	import java.util.Set;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.entity.Question;
 import com.utils.ConnUtils;
 
-	public class QuestionDao {
-		private Connection conn;
-		private Statement stmt;
-		private PreparedStatement pstmt;
-		private ResultSet rs;
-		private int ri,row,rc;
-		public ArrayList<Question> getQuestions() {
-			ArrayList<Question> result = new ArrayList<Question>();
-				try {
-					conn = ConnUtils.getConnection();//
-					pstmt = conn.prepareStatement("SELECT * FROM Question");
-					rs = pstmt.executeQuery();					
-					while (rs.next()){
-						Question question = new Question();
-						question.setId(rs.getInt("qID"));
-					    question.setContent(rs.getString("qContent"));
-						result.add(question);
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					ConnUtils.releaseConn(rs, pstmt, conn);
-				}
-				return result;
+public class QuestionDao {
+	private Connection conn;
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	private int ri, row, rc;
+
+	public ArrayList<Question> getQuestions() {
+		ArrayList<Question> result = new ArrayList<Question>();
+		try {
+			conn = ConnUtils.getConnection();//
+			pstmt = conn.prepareStatement("SELECT * FROM Question");
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Question question = new Question();
+				question.setId(rs.getInt("qID"));
+				question.setContent(rs.getString("qContent"));
+				result.add(question);
 			}
-	public Question getQuestionById(int id){
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnUtils.releaseConn(rs, pstmt, conn);
+		}
+		return result;
+	}
+
+	public Question getQuestionById(int id) {
 		Question question = new Question();
-		try{
+		try {
 			conn = ConnUtils.getConnection();//
 			String getQuestion = "SELECT * FROM Question WHERE qID = ?";
 			pstmt = conn.prepareStatement(getQuestion);
 			pstmt.setInt(1, id);
-			rs = pstmt.executeQuery();	
+			rs = pstmt.executeQuery();
 			if (rs.next()) {
-					question = new Question();
-					question.setId(id);
-					question.setContent(rs.getString("qContent"));
+				question = new Question();
+				question.setId(id);
+				question.setContent(rs.getString("qContent"));
 			}
-			} catch (SQLException e) {
-				e.printStackTrace(); 
-			} finally {
-				ConnUtils.releaseConn(rs, pstmt, conn);
-			}
-			return question;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnUtils.releaseConn(rs, pstmt, conn);
+		}
+		return question;
+	}
+
+	public void addQuestion(Question q) {
+		Session session = HibernateSessionFactory.getSession();
+		Transaction t = session.beginTransaction();
+		try {
+			session.save(q);
+			t.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			t.rollback();
+		} finally {
+			HibernateSessionFactory.closeSession();
 		}
 	}
+}
